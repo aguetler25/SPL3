ad1fec75286b73d70c183c25992a0ecb2d7c9bb8
aguetler25 <aguetler25@icapital.at>
2018-04-25 12:27:03 +0200

    zeiten mit parameter aufrufbar

--- zeiten.py
+++ zeiten.py
@@ -1,5 +1,7 @@
 # zeiten.py 
 
+import sys
+
 def zeitInSekunden(h, m, s):
     gesamt = 0
     gesamt += h * 3600
@@ -7,20 +9,21 @@ def zeitInSekunden(h, m, s):
     gesamt += s
     return gesamt
 
-beginnZeit = input("Beginnzeit: ")
-# endeZeit = input("Endezeit: ")
+beginnZeit = sys.argv[1] # input("Beginnzeit: ")
+endeZeit = sys.argv[2] # input("Endezeit: ")
 
 # Zeit im Format HH:MM:SS
 beginn = beginnZeit.split(":")
-print (beginn)
-
+ende = endeZeit.split(":")
 h = int(beginn[0])
 m = int(beginn[1])
 s = int(beginn[2])
-
-print("Stunden :", h)
-print("Minuten :", m)
-print("Sekunden:", s)
-
 beginnSekunden = zeitInSekunden(h, m, s)
-print("Gesamt Sekunden Beginn:", beginnSekunden)
+
+h = int(ende[0])
+m = int(ende[1])
+s = int(ende[2])
+endeSekunden = zeitInSekunden(h,m,s)
+
+gesamt = endeSekunden - beginnSekunden
+print(gesamt)
