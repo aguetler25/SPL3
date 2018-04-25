523aaf1ceea2cf527848d556be61ebc5210cb007
aguetler25 <aguetler25@icapital.at>
2018-04-25 08:59:19 +0200

    Eingabe der Haltestellen...

--- parkhaus.py
+++ parkhaus.py
@@ -1,2 +1,14 @@
 # parkhaus.py 
 # Angabe fuer das Beispiel: siehe moodle
+
+print ("Linienbus-Simulator")
+
+einsteiger = input("Wie viele Personen wollen einsteigen")
+
+personen = int(einsteiger)
+
+for h in range (1,5):
+    print("Guten Morgen!", h)
+
+print("ende.")
+
